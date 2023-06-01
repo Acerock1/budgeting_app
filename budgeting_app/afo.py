cc641c5b8627380eb900185f7dea6606d7ca9232
Afajobi <105067655+Acerock1@users.noreply.github.com>
2023-06-01 17:26:27 +0000

    update afo.py

--- budgeting_app/afo.py
+++ budgeting_app/afo.py
@@ -1,97 +1,115 @@
+#dependecies
 import streamlit as st
 from streamlit_option_menu import option_menu
 import plotly.graph_objects as go
 from datetime import date
 import calendar
-import database
+import database as db
 
-st.set_page_config(
-    page_title="budgeting_app",
-    page_icon="💵",
-    layout="centered"
-)
 
-# Page config
-incomes = ["Salary", "Blog", "Other Income"]
+# global variables
+incomes = ["Salary", "Youtube", "Car Business", "Other Income"]
 expenses = ["Rent", "Utilities", "Car", "Savings", "Other Expenses"]
 currency = "USD"
 page_title = "Income and Expense Tracker"
 page_icon = "💵"
-years = [date.today().year, date.today().year - 1, date.today().year - 2]
+years = [date.today().year, date.today().year-1, date.today().year-2]
 months = list(calendar.month_name[1:])
 
-#page layout--- title, and option menu for data entry and data visualization
-st.title(page_title +" "+ page_icon)
+#page configuration
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
 
 selected = option_menu(
     menu_title=None,
-    options= ["Data Entry", "Data Visualiztion"],
-    icons=("📝", "📊"),
-    orientation= "horizontal"
+    options=["Data Entry", "Data Visualization"],
+    icons=["pencil-fill", "bar-chart-fill"],  # https://icons.getbootstrap.com/
+    orientation="horizontal",
 )
 
-
-
-# Database interface
+# db interface
 def get_all_periods():
-    items = database.fetch_all_periods()
+    items = db.fetch_all_periods()
     periods = [item["key"] for item in items]
     return periods
 
 
-# Data entry column (option menu)
-if option == "Data Entry":
+# css to hide streamlit styles
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+
+
+# --- INPUT & SAVE PERIODS ---
+if selected == "Data Entry":
     st.header(f"Data Entry in {currency}")
-    with st.form("Enter Data", clear_on_submit=True):
+    with st.form("entry_form", clear_on_submit=True):
         col1, col2 = st.columns(2)
         col1.selectbox("Select Month:", months, key="month")
         col2.selectbox("Select Year:", years, key="year")
 
-        "_ _ _"
-        with st.expander("Incomes"):
+        "---"
+        with st.expander("Income"):
             for income in incomes:
-                st.number_input(f"{income}:", min_value=0, step=100, format="%i")
+                st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
         with st.expander("Expenses"):
             for expense in expenses:
-                st.number_input(f"{expense}:", min_value=0, step=50, format="%i")
+                st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
         with st.expander("Comment"):
-            comment = st.text_area("", placeholder="Enter your comments here 🥳...")
-        
-        "_ _ _"
+            comment = st.text_area("", placeholder="Enter a comment here ...")
+
+        "---"
         submitted = st.form_submit_button("Save Data")
         if submitted:
             period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
             incomes = {income: st.session_state[income] for income in incomes}
             expenses = {expense: st.session_state[expense] for expense in expenses}
-            database.insert_period(period, incomes, expenses, comment)
-            st.success("Data saved 🔥")
+            db.insert_period(period, incomes, expenses, comment)
+            st.success("Data saved!")
 
 
-# Data visualization option menu
-if option == "Data Visualization":
+# --- PLOT PERIODS ---
+if selected == "Data Visualization":
     st.header("Data Visualization")
-    with st.form("Saved Periods"):
+    with st.form("saved_periods"):
         period = st.selectbox("Select Period:", get_all_periods())
         submitted = st.form_submit_button("Plot Period")
         if submitted:
-            "_ _ _"
-            period_data = database.get_period(period)
+            # Get data from database
+            period_data = db.get_period(period)
             comment = period_data.get("comment")
-            incomes = period_data.get("incomes")
             expenses = period_data.get("expenses")
-            
+            incomes = period_data.get("incomes")
+
             # Create metrics
             total_income = sum(incomes.values())
-            total_expenses = sum(expenses.values())
-            remainder = total_income - total_expenses
+            total_expense = sum(expenses.values())
+            remaining_budget = total_income - total_expense
             col1, col2, col3 = st.columns(3)
-            col1.metric("Total Income", f"{total_income}{currency}")
-            col2.metric("Total Expense", f"{total_expenses}{currency}")
-            col3.metric("Remaining Budget", f"{remainder}{currency}")
+            col1.metric("Total Income", f"{total_income} {currency}")
+            col2.metric("Total Expense", f"{total_expense} {currency}")
+            col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
             st.text(f"Comment: {comment}")
 
             # Create sankey chart
             label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
             source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
+            target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
+            value = list(incomes.values()) + list(expenses.values())
 
-            # Rest of your visualization code...
+            # Data to dict, dict to sankey
+            link = dict(source=source, target=target, value=value)
+            node = dict(label=label, pad=20, thickness=30, color="#E694FF")
+            data = go.Sankey(link=link, node=node)
+
+            # Plot it!
+            fig = go.Figure(data)
+            fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
+            st.plotly_chart(fig, use_container_width=True)
