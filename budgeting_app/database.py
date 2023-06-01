7f97327d691d38da0456df59901a5a1be33034ae
Afajobi <105067655+Acerock1@users.noreply.github.com>
2023-06-01 18:32:45 +0000

    test

--- budgeting_app/database.py
+++ budgeting_app/database.py
@@ -12,7 +12,7 @@ def insert_period(period, income, expenses, comment):
         TableName=TableName,
         Item={
             'period': {'S': period},
-            'income': {'N': income},
+            'incomes': {'N': income},
             'expenses': {'N': expenses},
             'comment': {'S': comment}
         }
@@ -46,4 +46,4 @@ income = 6000
 expenses = 4500
 comment = "null"
 
-insert_period(period, income, expenses, comment)
+insert_period(period = period, income= incomes, expenses = expenses, comment= comment)
